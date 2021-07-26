4e95f220b9ec2d0e08a030556fed74c4d4406cf2
ramizkazak <ramizkazak@gmail.com>
2021-07-26 01:26:49 -0500

    Add and configurated pom.xml file

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\Users\ramiz\.jenkins\workspace\practiceJenkins
+set projectLocation=C:\Users\ramiz\eclipse-workspace\RestAssuredAPIWithExcel
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
